55974ddab969f807d61535340924e74534060891
yoramclapper <yoram.t.c@gmail.com>
2025-10-07 11:35:40 +0200

    update settings

--- buddyread/buddyread/settings.py
+++ buddyread/buddyread/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['buddyread.nl', 'www.buddyread.nl']
 
 
 # Application definition
