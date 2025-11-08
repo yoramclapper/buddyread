91d2e301191881b056b696ade2d58cffa80ea381
yoramclapper <yoram.t.c@gmail.com>
2025-11-08 08:51:37 +0100

    add | book club models

--- buddyread/books/admin.py
+++ buddyread/books/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-
-from .models import Book, Review
+from .models import Book, Review, BookClub, BookClubMembers, BookClubBooks
 
 admin.site.register(Book)
 admin.site.register(Review)
+
+class BookClubMembersInline(admin.TabularInline):
+    model = BookClubMembers
+    extra = 1
+
+@admin.register(BookClub)
+class BookClubAdmin(admin.ModelAdmin):
+    list_display = ('naam', 'creation_date')
+    inlines = [BookClubMembersInline]
+
--- buddyread/books/models.py
+++ buddyread/books/models.py
@@ -40,3 +40,29 @@ class Review(models.Model):
 
     def __str__(self):
         return f"{self.book.title} - {self.user.username}"
+
+
+class BookClub(models.Model):
+    naam = models.CharField(max_length=255, null=False, blank=False)
+    creation_date = models.DateField(auto_now_add=True)
+    end_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.naam}"
+
+
+class BookClubMembers(models.Model):
+    book_club = models.ForeignKey(BookClub, on_delete=models.CASCADE, blank=False, null=False)
+    member = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False)
+
+    class Meta:
+        verbose_name_plural = "Book club members"
+
+
+class BookClubBooks(models.Model):
+    book_club = models.ForeignKey(BookClub, on_delete=models.CASCADE, blank=False, null=False)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, blank=False, null=False)
+
+    class Meta:
+        verbose_name_plural = "Book club books"
+
